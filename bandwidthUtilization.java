aaad3aa4809bc41574f9ab4541c7b2bc4679cba9
Nick Luong <nckluong@gmail.com>
2020-06-28 14:40:50 -0400

    finished bandwidth utilization

--- bandwidthUtilization.java
+++ bandwidthUtilization.java
@@ -4,20 +4,18 @@ import java.util.*;
 public class serverInfo {
 
     // server instance variables
-    public String serverName;
-    public String interfaceName;
-    public double bandwidth;
+    private String serverName;
+    private String interfaceName;
+    private double bandwidth;
 
     // bandwidth utilization variables
     public List<serverInfo> serverList;
     public List<List<String>> bandwidths;
-    public double bandwidthUtilization;
 
     public serverInfo() {
         this.serverName = "";
         this.interfaceName = "";
         this.bandwidth = 0.0;
-        this.bandwidthUtilization = 0.0;
     }
 
     // setters
@@ -48,14 +46,11 @@ public class serverInfo {
         return this.bandwidth;
     }
 
-    public double getBandwidthUtilization() {
-        return this.bandwidthUtilization;
-    }
-
     public List<serverInfo> getServerList(){
         return this.serverList;
     }
 
+    // creates arraylist of bandwidth info
     public void getBandwidths() {
         List<List<String>> records = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File("data/bandwidth.csv"));) {
@@ -70,6 +65,7 @@ public class serverInfo {
         this.bandwidths = records;
     }
 
+    // csv line reading helper
     private List<String> getRecordFromLine(String line) {
         List<String> values = new ArrayList<String>();
         try (Scanner rowScanner = new Scanner(line)) {
@@ -81,10 +77,46 @@ public class serverInfo {
         return values;
     }
 
+    //create arraylist of netbitrate info
+    //loop through netbitrates, calculating and printing utilization every line
+    public void printBandwidthUtilization() {
+        List<List<String>> netbitrates = new ArrayList<>();
+        List<serverInfo> serverInfoList = this.getServerList();
+        try (Scanner scanner = new Scanner(new File("data/netbitrate.csv"));) {
+            while (scanner.hasNextLine()) {
+                netbitrates.add(getRecordFromLine(scanner.nextLine()));
+            }
+            netbitrates.remove(0); // remove first line
+            for (List<String> val : netbitrates){
+               String serverName = val.get(1);
+               String interfaceName = val.get(2);
+               double netbitrate = Double.parseDouble(val.get(3));
+               double bandwidth = findServerBandwidth(serverInfoList, serverName, interfaceName);
+               val.set(3, netbitrate/bandwidth+"");
+               System.out.println(val);
+            }
+        }
+        catch (Exception e){
+            System.out.println("File not found");
+        }
+    }
+
+    // return bandwidth of specific server by checking server name and interface name
+    public double findServerBandwidth(List<serverInfo> serverList, String serverName, String interfaceName) {
+        for(serverInfo s : serverList){
+             if(s.getServerName().equals(serverName) && s.getInterfaceName().equals(interfaceName)){
+                 return s.getBandwidth();
+             }
+        }
+        return 0.0;
+    }
+
+
     // takes in list of server bandwidth info and generates list of server info
-    public void generateServerList(List<List<String>> bandwidths){
+    // makes checking server properties easier while matching up server and interface names
+    public void generateServerList(){
         List<serverInfo> servers = new ArrayList<serverInfo>();
-        for (List<String> val : bandwidths){
+        for (List<String> val : this.bandwidths){
             serverInfo newServer = new serverInfo();
             newServer.setServerName(val.get(0));
             newServer.setInterfaceName(val.get(1));
@@ -95,16 +127,12 @@ public class serverInfo {
     }
 
 
+    //creates serverInfo instance, generates list of serverInfo from reading bandwidth.csv, prints calculated bandwidth utilization
     public static void main(String[] args) {
         serverInfo server = new serverInfo();
         server.getBandwidths();
-        server.generateServerList(server.bandwidths);
-        for(serverInfo s: server.getServerList()){
-            System.out.println("---------------------------");
-            System.out.println(s.serverName);
-            System.out.println(s.interfaceName);
-            System.out.println(s.bandwidth);
-        }
+        server.generateServerList();
+        server.printBandwidthUtilization();
     }
 
 }
